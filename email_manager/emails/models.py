041c5dc8dda09f3556ca495e7211297426eee45c
Hju Kneyck Flores <hjucode@gmail.com>
2021-06-22 20:12:36 +0800

    fix: define primary keys as per django 3.2

--- email_manager/emails/models.py
+++ email_manager/emails/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Email(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254, unique=True)
     owner = models.ForeignKey(User,
